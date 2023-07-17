ac6cfa495cb232fe5dcefbc05000eced6b2b662c
AnaMCarrenoA <128761853+AnaMCarrenoA@users.noreply.github.com>
2023-07-16 20:30:50 -0400

    colaborador-3

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # test_flask_1_1
+
+Para eliminar un mensaje exitosamente, envía una solicitud DELETE al endpoint `/read`. La respuesta será un objeto JSON con el mensaje `"read successfully deleted"`.
--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return jsonify({"payload": "welcome to my project"})
 
+@app.route("/delete", methods=["DELETE"])
+def delete():
+    return jsonify ({"payload":"delete successfully"})
+
 if __name__ == '__main__':
     app.run(debug=True)
